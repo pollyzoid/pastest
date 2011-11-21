9b0b0e30ec5b6b178e59aafc1a8ec7e7a228466b
Aleksi Kervinen <pollyzoid@gmail.com>
2011-11-21 16:10:50 +0200

    added actual pasting

---
+++ lib/pastest/paste.rb
@@ -0,0 +1,8 @@
+class Paste
+	attr_reader :id, :body
+
+  def initialize(id, body)
+    @id = id
+    @body = body
+  end
+end
--- pastest.rb
+++ pastest.rb
@@ -4,23 +4,39 @@ require 'haml'
 require 'data_mapper'
 require 'json'
 
+$: << File.join(File.dirname(__FILE__), 'lib')
+require 'pastest/paste'
+
 #DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-set :haml, :format => :html5
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
 
-pastes = []
+set :haml, :format => :html5
+set :haml, :escape_html => true
+
+@@pastes = Array.new
 
 get '/', :provides => :html do
+  puts @@pastes.nil?
   haml :index
 end
 
 get '/:id', :provides => :html do |id|
-  @id = id
+  id = id.to_i
+
+  halt 404, haml(:nopaste) if @@pastes[id-1].nil?
+
+  @paste = @@pastes[id-1]
+
   haml :paste
 end
 
-post '/' do
-
+post '/', :provides => :html do
+  @@pastes << Paste.new(@@pastes.count + 1, params[:body])
+  redirect "/#{@@pastes.count}"
 end
 
 delete '/' do
