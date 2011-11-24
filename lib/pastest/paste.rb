4a478644fbcf3a11588836cc237d8f8a51e30b1f
Aleksi Kervinen <pollyzoid@gmail.com>
2011-11-24 19:45:38 +0200

    fixed languages

--- lib/pastest/paste.rb
+++ lib/pastest/paste.rb
@@ -1,6 +1,12 @@
 require 'dm-core'
 require 'dm-types'
 
+# bleh
+LANG_ENUM = []
+LANGUAGES.each do |k, v|
+  LANG_ENUM << k
+end
+
 class Paste
   include DataMapper::Resource
 
@@ -9,7 +15,7 @@ class Paste
   property :updated_at, DateTime
   property :private,    Boolean,  :default => false
   property :body,       Text,     :required => true
-  property :language,   Enum[*LANGUAGES], :default => :plain
+  property :language,   Enum[*LANG_ENUM], :default => :lua
 
   def self.public
     all(:private => false)
